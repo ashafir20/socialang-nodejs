449d7baa7d207605e498c0cb2e664de3404bafed
Aviv Shafir <ashafir20@gmail.com>
2014-08-20 21:15:17 +0300

    asdasdasdad

--- controllers/Chat/AdvancedChat.js
+++ controllers/Chat/AdvancedChat.js
@@ -16,22 +16,24 @@ var colors = require('colors');
 
 
 exports.HandleAdvancedChat = function (socket, io) {
-	socket.on('JoinAdvancedChatRequest', function (data){
+	socket.on('JoinAdvancedChatRequest', function (data)
+	{
 		console.log("in JoinAdvancedChatRequest");
-		socket.get('id', function (err, id) {
+		socket.get('id', function (err, id) 
+		{
 			if(id){
 				User.findById(id, function (error, user) {
 					if(user) {
 						console.log('found user!'.blue);
 						socket.join('main');
 						//store the room name in the socket session for this client
-						socket.room = 'main';
+						socket.set('room', 'main');
 						var fullname = user.firstName + " " + user.lastName;
 
 						var jsonResponse = { 
 							 result: 'OK', 
 							 sender : 'SERVER',
-							 message : fullname + ' Welcome to the chat!',
+							 message : 'Welcome ' + fullname,
 							 date : '05:13'
 							};
 
@@ -47,7 +49,30 @@ exports.HandleAdvancedChat = function (socket, io) {
 					}
 				});
 			}
-		})
+		});
 	});
+
+	socket.on('GetAdvancedChatOnlineUsers', function () {
+		var users = io.sockets.clients('main');
+		var onlineusers = [];
+		for (var socketId in io.sockets.sockets) 
+		{
+			getUserDetails(socketid, function (userDetails) {
+				onlineusers.push();
+			});
+		}
+	});
+
+	function getUserDetails(socketid, callback){
+	    io.sockets.sockets[socketId].get('id', function (err, userid) {
+        if(userid){
+        	User.findById(id, function (error, user) {
+	        		if(user) {
+	        			
+	        		}
+	        	}
+	        }
+	    });
+	}
 }
 
